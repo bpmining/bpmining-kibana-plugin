012c98aeea82d4541727d48817aebbc264329072
Luc Weinbrecht <luc.weinbrecht@novatec-gmbh.de>
2022-05-23 13:26:59 +0200

    added prettier to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['@kbn/eslint-config', 'plugin:@elastic/eui/recommended'],
+  extends: ['@kbn/eslint-config', 'plugin:@elastic/eui/recommended', 'prettier'],
   rules: {
     '@kbn/eslint/require-license-header': 'off',
     quotes: 'single',
+    'no-console': 1, // Means warning
+    'prettier/prettier': 2, // Means error
   },
+  plugins: ['prettier'],
 };
