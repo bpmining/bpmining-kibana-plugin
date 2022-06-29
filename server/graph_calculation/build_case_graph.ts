50a648b23684a4da5d367318c11e4d24b311b41e
Schlangstedt Anne <asg@novatec-gmbh.de>
2022-06-29 18:23:46 +0200

    added start and endpoint to all graphs

--- server/graph_calculation/build_case_graph.ts
+++ server/graph_calculation/build_case_graph.ts
@@ -1,5 +1,7 @@
 import { ProcessEvent } from 'plugins/bpmining-kibana-plugin/model/process_event';
 import { VisEdge, VisNode } from 'plugins/bpmining-kibana-plugin/model/vis_types';
+import { addStartAndEndPoint } from '../helpers/add_start_end_point';
+import { getNeighboursFor } from '../helpers/get_node_neighbours';
 import { assignNodeIds } from './assign_node_ids';
 import { calculateEdges } from './calculate_edges';
 import { calculateNodeThroughputTime } from './calculate_node_throughput_time';
@@ -13,7 +15,11 @@ export interface ProcessGraph {
 export function buildCaseGraph(nodes: ProcessEvent[]) {
   const sortedNodes = sortNodes(nodes, 'timestamp');
   const nodesWithIds: VisNode[] = assignNodeIds(sortedNodes);
-  const edges = calculateEdges(nodesWithIds);
+  const nodesWithNeighbours = getNeighboursFor(nodesWithIds);
+  const nodesWithEndpoints = addStartAndEndPoint(nodesWithNeighbours);
+
+  const finalNodes = nodesWithEndpoints.map((item) => item.node);
+  const edges = calculateEdges(nodesWithEndpoints);
 
   for (let node of nodesWithIds) {
     const throughputTime = calculateNodeThroughputTime(node);
@@ -24,7 +30,7 @@ export function buildCaseGraph(nodes: ProcessEvent[]) {
   }
 
   const graph = {
-    nodes: nodesWithIds,
+    nodes: finalNodes,
     edges: edges,
   };
 
--- server/graph_calculation/calculate_edges.ts
+++ server/graph_calculation/calculate_edges.ts
@@ -1,18 +1,28 @@
-import { VisNode, VisEdge } from 'plugins/bpmining-kibana-plugin/model/vis_types';
+import { VisEdge } from 'plugins/bpmining-kibana-plugin/model/vis_types';
+import { VisNodeNeighbours } from './build_aggregated_graph';
 
-export function calculateEdges(visNodes: VisNode[]) {
+export function calculateEdges(aggregatedNodes: VisNodeNeighbours[]) {
   let edges: VisEdge[] = [];
 
-  for (let i = 0; i < visNodes.length; i++) {
-    if (i === 0) {
-      continue;
+  aggregatedNodes.forEach((node: VisNodeNeighbours) => {
+    if (node.next.id === undefined || node.prev.id === undefined) {
+      return;
     }
-    const edge = {
-      from: visNodes[i - 1].id,
-      to: visNodes[i].id,
+    const edgeToNext = {
+      from: node.node.id,
+      to: node.next.id,
     };
-    edges.push(edge);
-  }
+    edges.push(edgeToNext);
 
-  return edges;
+    const edgeToPrevious = {
+      from: node.prev.id,
+      to: node.node.id,
+    };
+    edges.push(edgeToPrevious);
+  });
+
+  const uniqueEdges = edges.filter(
+    (edge, index, self) => index === self.findIndex((i) => i.from === edge.from && i.to === edge.to)
+  );
+  return uniqueEdges;
 }
