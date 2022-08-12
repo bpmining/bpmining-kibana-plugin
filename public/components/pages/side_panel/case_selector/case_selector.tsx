e4d72fe3eb387f4c50c6c4815aabc0e232fae9d9
Schlangstedt Anne <asg@novatec-gmbh.de>
2022-08-12 10:23:53 +0200

    improved styling of autocomplete component

--- public/components/pages/side_panel/case_selector/case_selector.tsx
+++ public/components/pages/side_panel/case_selector/case_selector.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, TextField, Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { AnyAction, bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { RootReducer } from '../../../../reducer/root_reducer';
+import { NODE_COLOR_LAYER_1, NODE_COLOR_LAYER_2 } from '../../../../../common/colors';
 
 import * as fetchCaseGraphActions from '../../../../reducer_actions/fetch_case_specific_graph';
+import { calculateColorValue } from '../../../../services';
 
 interface CaseSelectorState {
   rootReducer: RootReducer;
@@ -25,12 +28,42 @@ const mapStateToProps = (state: CaseSelectorState) => {
   return state;
 };
 
+interface StyleProps {
+  hoverColor: string;
+}
+
+const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) => ({
+  listbox: {
+    '& .MuiAutocomplete-option[aria-selected="true"]': {
+      backgroundColor: 'white',
+    },
+    '& .MuiAutocomplete-option[aria-selected="true"].Mui-focused': {
+      backgroundColor: ({ hoverColor }) => hoverColor,
+    },
+    '& .MuiAutocomplete-option[aria-selected="false"]': {
+      backgroundColor: 'white',
+    },
+    '& .MuiAutocomplete-option[aria-selected="false"].Mui-focused': {
+      backgroundColor: ({ hoverColor }) => hoverColor,
+    },
+  },
+}));
+
 const CaseSelector = (props: CaseSelectorProps) => {
   const [value, setValue] = useState<CaseSelectorOption | null>({ label: '' });
 
-  useEffect(() => {}, [props]);
+  useEffect(() => {}, [props.rootReducer.layer.selectedLayer]);
 
   const cases: CaseSelectorOption[] = [];
+  const borderColor = calculateColorValue(props.rootReducer.layer.selectedLayer);
+  const hoverColor =
+    props.rootReducer.layer.selectedLayer === 1 ? NODE_COLOR_LAYER_1 : NODE_COLOR_LAYER_2;
+
+  const styleProps = {
+    hoverColor: hoverColor,
+  };
+
+  const classes = useStyles(styleProps);
 
   for (let i = 0; i < props.caseIds.length; i++) {
     cases.push({ label: props.caseIds[i] });
@@ -49,14 +82,30 @@ const CaseSelector = (props: CaseSelectorProps) => {
 
   return (
     <Autocomplete
+      classes={classes}
       value={value}
       isOptionEqualToValue={(option: any, value: any) => option.value === value.value}
       disablePortal
-      id="combo-box-demo"
+      id="combo-box"
       options={cases}
       sx={{ width: '100%' }}
       onChange={(event, value) => handleChange(event, value)}
-      renderInput={(params) => <TextField {...params} label="Select one case" />}
+      renderInput={(params) => (
+        <TextField
+          sx={{
+            '& label.Mui-focused': {
+              color: borderColor,
+            },
+            '& .MuiOutlinedInput-root': {
+              '&.Mui-focused fieldset': {
+                borderColor: borderColor,
+              },
+            },
+          }}
+          {...params}
+          label="Select one case"
+        />
+      )}
     />
   );
 };
